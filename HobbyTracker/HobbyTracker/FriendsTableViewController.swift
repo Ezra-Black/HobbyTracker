b4422140f429051d798e2af80fa3d6be69dcd240
JosephRogers94 <Joerogersss2016@gmail.com>
2019-09-27 00:39:55 -0500

    Added Files

---
+++ HobbyTracker/HobbyTracker/FriendsTableViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FriendsTableViewController.swift
+//  HobbyTracker
+//
+//  Created by Joseph Rogers on 9/27/19.
+//  Copyright © 2019 Joe Rogers. All rights reserved.
+//
+
+import UIKit
+
+class FriendsTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
