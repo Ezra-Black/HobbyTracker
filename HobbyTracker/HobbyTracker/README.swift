7c3af34d83aa8528cae239ec8521b97063c19173
JosephRogers94 <Joerogersss2016@gmail.com>
2019-09-28 22:47:45 -0500

    README in files added

---
+++ HobbyTracker/HobbyTracker/README.swift
@@ -0,0 +1,23 @@
+//
+//  README.swift
+//  HobbyTracker
+//
+//  Created by Joseph Rogers on 9/28/19.
+//  Copyright © 2019 Joe Rogers. All rights reserved.
+//
+/* MARK: Read Me
+ 
+ Hello! I am Joseph Rogers. And I am in iOSPT3 in Lambda school. I highly encourage anybody seeing this to gladly update the notes as seen fit, and to gladly submit a pull request! This version of Hobby Tracker is meant to guide you through the steps for the logic of making a simple application!
+ 
+ you can find the github and the youtube video to this guide here:
+
+
+ https://github.com/JosephRogers94/HobbyTracker
+ 
+ 
+ 
+ 
+ -Thanks!
+ 
+ 
+ */
